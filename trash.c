48d977384d6c90fcb20bc363218cfd6b62505185
ninja542 <theroboticcyborg@gmail.com>
2017-03-31 10:46:35 -0400

    added pseudo code

--- trash.c
+++ trash.c
@@ -1,3 +1,8 @@
 task main(){
-  
+  int radius = 16; // radius of trash from waterbottle
+  /*
+  the robot will go around in a circle and push all the bases, and return to home base
+  then the robot will go around the waterbottle in a square to clear the trash
+  basically the robot pushs the trash all the way outside, goes backwards, turns left, and repeats
+  */
 }
